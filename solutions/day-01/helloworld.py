f0b4e57f10637473c0e5d205c83af50f18d91464
Fiqih Nur Muharrom <fiqih.nur@grabtaxi.com>
2021-04-01 09:11:40 +0700

    chore: complete exercise day 01

---
+++ solutions/day-01/helloworld.py
@@ -0,0 +1,25 @@
+import sys
+
+# print python version
+print('Python Version')
+print(sys.version)
+
+print('\nOperation')
+print(3 + 4)    # addition (+)
+print(3 - 4)    # subtraction (-)
+print(3 * 4)    # multiplication (*)
+print(3 % 4)    # modulus (%)
+print(3 / 4)    # division (/)
+print(3 ** 4)   # exponential (**)
+print(3 // 4)   # floor division operator (//)
+
+print('\nType Check')
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['muharrom', 'python', 'Indonesia']))
+print(type({
+    'name':'muharrom',
+    'country': 'Indonesia'
+    }))
